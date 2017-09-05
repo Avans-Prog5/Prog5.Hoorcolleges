9ef17b8b851073d87d48ec03466272cdde703414
Stino <linksonder@gmail.com>
2017-09-05 15:22:06 +0200

    hoorcollege week 1

---
+++ Hoorcolleges/Hoorcollege.week2/AgeCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoorcollege.week2
+{
+    public class AgeCalculator
+    {
+        public int Calculate()
+        {
+            //1. Input van een gebruiker
+            string input = Console.ReadLine();
+
+            DateTime result;
+
+            //2. De input omzetten naar datum
+            //Je MOET het woordje out voor de 2de parameter zetten
+            while (!DateTime.TryParse(input, out result))
+            {
+                input = Console.ReadLine();
+            }
+
+            //3. Leeftijd berekenen
+            int age = DateTime.Now.Year - result.Year;
+
+            Console.WriteLine(age);
+            Console.ReadLine();
+            return age;
+
+        }
+    }
+}
---
+++ Hoorcolleges/Hoorcolleges/Program.cs
@@ -0,0 +1,27 @@
+﻿using Hoorcollege.week2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoorcolleges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AgeCalculator calcy = new AgeCalculator();
+            //calcy.Calculate();
+
+            var namen = new List<String>() { "STijn", "Bart", "Erik", "Bram", "Suzanne" };
+
+            var metB = from naam in namen where naam[0] == 'B';
+        }
+
+        static public Boolean HasFIrstLetterB(string i)
+        {
+            return i[0] == 'B';
+        }
+    }
+}
