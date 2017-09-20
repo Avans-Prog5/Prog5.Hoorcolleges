32810bca5785ebe07b54b626c3a033420b648906
Stino <linksonder@gmail.com>
2017-09-20 12:32:41 +0200

    Tonen van een lijstje

---
+++ Hoorcolleges/Hoorcollege.Week3/ViewModel/MemeListVM.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoorcollege.Week3
+{
+    public class MemeListVM : ViewModelBase
+    {
+        public List<Meme> Memes { get; set; }
+
+        public MemeListVM()
+        {
+            Memes = new List<Meme>();
+            Memes.Add(new Meme() { Naam = "Trump", Categorie = "gek haar" });
+            Memes.Add(new Meme() { Naam = "Grumpy cat", Categorie = "classics" });
+
+        }
+    }
+}
